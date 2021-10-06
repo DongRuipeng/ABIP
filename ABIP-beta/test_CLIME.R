86418634c14cddc7582614c1fd79b8f5235bdea3
Ruipeng <drpoct@163.com>
2021-10-06 11:06:32 +0800

    add test_CLIME file for comparision

---
+++ ABIP-beta/test_CLIME.R
@@ -0,0 +1,28 @@
+require(MASS)
+setwd("D:/Git/ABIP")
+
+n <- 100
+p <- 80
+rho <- 0.5
+Sigma_0 <- rho ^ abs(outer(1:p, 1:p, "-"))
+Omega_0 <- solve(Sigma_0)
+X <- mvrnorm(n, rep(0, p), Sigma_0)
+Sigma_n <- t(X) %*% X / n
+
+lambda <- c(1e-3, 1e-2, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8)
+nlambda <- length(lambda)
+Omega <- rep(0, nlambda * p * p)
+time <- 0
+
+dyn.load("./ABIP-beta/src/abip.dll")
+res <- .C("abip_R", as.double(Sigma_n), as.integer(p), as.double(lambda), as.integer(nlambda), Omega = as.double(Omega), time = as.double(time))
+dyn.unload("./ABIP-beta/src/abip.dll")
+
+Omega <- matrix(res$Omega, p, nlambda * p)
+time <- res$time
+
+for (l in c(1:nlambda)) {
+  error <- norm(Omega[, ((l - 1) * p + 1):(l * p)] - Omega_0, "F") / norm(Omega_0, "F")
+  cat("The error of Omega[",l,"]: ",error,"\n", sep = "")
+}
+cat("the time is ",time," secs.\n")
--- Original-ABIP/abip/test_CLIME.m
+++ Original-ABIP/abip/test_CLIME.m
@@ -1,43 +1,43 @@
+clc
 rho = 0.5;
 n = 100;
-p = 100;
+p = 80;
 Sigma_0 = rho .^ abs((1:p) - (1:p)');
 Omega_0 = inv(Sigma_0);
 X = mvnrnd(zeros(1,p),Sigma_0, n);
 Sigma_hat = X' * X / n;
 
-lambda = 1e-3;
-Basics = eye(p);
-
-likelihood = @(Sigma, Omega) ...
-    (Sigma(:))' * Omega(:) - log(det(Omega));
+lambda = [1e-3,1e-2,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8];
+nlambda = length(lambda);
+Omega = zeros(p,p,nlambda);
 
 A = [Sigma_hat, -Sigma_hat, eye(p), zeros(p);...
     zeros(p), zeros(p), eye(p), eye(p)];
 A = sparse(A);
 c = [ones(p,1); ones(p,1); zeros(p,1); zeros(p,1)];
+Basics = eye(p);
 
 data.A = A;
 data.c = c;
 
-% lbounds = zeros(4*p,1);
-% ubounds = Inf(4*p,1);
-
-% [A,b,c,info] = presolve(A,b,c,lbounds,ubounds);
-% abips setting.
-data.A = sparse(A);
-data.c = full(c);
-%     params_abips = struct('max_iters', 100000, 'max_outiters', 10000);
-params_abips = struct("verbose",0);
 
 % abips implementation.
 tic;
 for j=1:p
-    b = [lambda * ones(p,1) + Basics(:,1);...
-        2*lambda*ones(p,1)];
-    data.b = full(b);
-     [x, y, s, info_abips] = abip_direct(data, params_abips); 
+    params_abips = struct("verbose",0);
+    for l = 1:nlambda
+        b = [lambda(l) * ones(p,1) + Basics(:,j);...
+            2*lambda(l)*ones(p,1)];
+        data.b = full(b);
+        [x, y, s, info_abips] = abip_direct(data, params_abips);
+        params_abips = struct("verbose",0, "x",x,"y",y,"s",s);
+        Omega(:,j,l) = x(1:p) - x((p+1):2*p);
+    end
 end
 time_abips = toc;
+for l=1:nlambda
+    fprintf("The error of Omega[%i]: %f\n", ...
+        l, norm(Omega(:,:,l)-Omega_0,'fro')...
+        / norm(Omega_0,'fro'));
+end
 fprintf("the time is %f secs.\n", time_abips);
-%     [x_abips, objp_abips] = postsolve(x, info);
